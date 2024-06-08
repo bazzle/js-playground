2cd1ab2aea165479c6cee00c0128a2af3b23b8ec
Barry Richards <barry@br-web.me>
2024-06-08 15:50:42 +0100

    Add coffee shop app

---
+++ coffee-shop/main.js
@@ -0,0 +1,53 @@
+const container = document.querySelector('.container');
+
+const coffeeRange = [
+    {
+        'name': 'Colombia El Carmen',
+        'type':'ground',
+        'origin': 'Colombia',
+        'price': '£6.75',
+        'inStock': false
+    },
+    {
+        'name': 'Brazilian Campestre no 109 ',
+        'type':'ground',
+        'origin': 'Brazil',
+        'price': '£8.22',
+        'inStock': false
+    },
+    {
+        'name': 'Swiss Water Decaf blend no11',
+        'type':'ground',
+        'origin': 'Switzerland',
+        'price': '£6.75',
+        'inStock': false
+    },
+    {
+        'name': 'Mocha Java Blend no3',
+        'type':'ground',
+        'origin': 'Java',
+        'price': '£8.22',
+        'inStock': true
+    }
+];
+
+let outOfStock = 0;
+
+coffeeRange.forEach((i) => {
+    if (i.inStock === false){
+        outOfStock++;
+    }
+});
+
+if(coffeeRange.length > 0 && outOfStock != coffeeRange.length ){
+    const list = document.createElement('ul');
+    container.appendChild(list);
+    coffeeRange.forEach((i) => {
+        const listItem = document.createElement('li');
+        let priceDisplay = i.inStock ? i.price : "Sorry we're out of stock";
+        listItem.innerHTML = `${i.name} – ${priceDisplay}`;
+        list.appendChild(listItem);
+    })
+} else {
+    container.innerHTML = "We are out I'm afraid";
+}
